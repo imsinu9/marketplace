f3fd625c73f005b794712f7d3993c18b7a7c27de
Prateek Mittal <imsinu9@gmail.com>
2013-10-27 05:26:16 +0530

    Adding field active to check active category adding method to know total product category hold

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,21 +3,30 @@ class Category
   include Mongoid::Timestamps
 
   field :name, type: String
+  field :active, type: Boolean, :default => true
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  scope :active, where(:active => true)
+
+  class<<self
+    def total_categories
+      Category.active.count
+    end
+  end
 
   def category_url
     "#{Marketplace::API::BASE_URL}/#{Marketplace::API::routes[0].route_version}/store/category/#{self.id}"
   end
 
+  def total_products
+    Product.tagged_with(:categories, self.name).count
+  end
+
   def as_json(options={})
     only = options[:only] || []
     methods = options[:methods] || []
-    super(:only => only.push(:name), :methods => methods.push(:category_url))
-  end
-
-  def total_products
-    Product.tagged_with(:categories,self.name).count
+    super(:only => only.push(:name), :methods => methods.push(:total_products))
   end
 end
